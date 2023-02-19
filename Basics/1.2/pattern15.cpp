73b5dd7ca8f2eba01a58d7040066ee32454d7f54
Kapil Soni <kapilsoni54768161@gmail.com>
2023-02-19 17:06:36 +0530

    .

---
+++ Basics/1.2/pattern15.cpp
@@ -0,0 +1,46 @@
+// Pattern :
+// ABCDE
+// ABCD
+// ABC
+// AB
+// A
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    void printTriangle(int n)
+    {
+        // code here
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 'A'; j < ('A' + n - i); j++)
+            {
+                cout << char(j);
+            }
+            cout << endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
