e0f0743fac57127e4689cfcb603f9d171d9c807e
Kapil Soni <kapilsoni54768161@gmail.com>
2023-02-21 20:38:49 +0530

    .

---
+++ 3. Arrays/3.1/Rotating an Array.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+public:
+    void leftRotate(int arr[], int n, int k)
+    {
+        // code here
+        vector<int> vec(n);
+        for (int i = 0; i < n; i++)
+        {
+            vec[i] = arr[(i + k) % n];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = vec[i];
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, d;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        cin >> d;
+        Solution ob;
+        ob.leftRotate(arr, n, d);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
