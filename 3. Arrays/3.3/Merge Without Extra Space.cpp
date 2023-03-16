c9b3a73b25eeb69269cd831f2c666d696459c6c4
Kapil <kapilsoni54768161@gmail.com>
2023-03-16 12:22:02 +0530

    .

---
+++ 3. Arrays/3.3/Merge Without Extra Space.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            // code here   
+           int ptr1 = 0 , ptr2=0;
+            
+            
+            while(ptr1<n && ptr2<m){
+                if(arr1[ptr1] > arr2[ptr2]){
+                    swap(arr1[ptr1] , arr2[ptr2]);
+                    sort(arr2 , arr2+m);
+                }
+                    ptr1++;
+            }
+        } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+
+// } Driver Code Ends
