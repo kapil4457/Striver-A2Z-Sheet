a38e8202468658652336e9c6a18213b19f05ac46
Kapil <kapilsoni54768161@gmail.com>
2023-03-26 12:13:48 +0530

    .

---
+++ 5. Strings/5.1/Aggressive Cows.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+bool isPossible(vector<int>&stalls , int k , int mid){
+    int cowCount = 1;
+    int lastPos = stalls[0];
+    int n = stalls.size();
+    
+    for(int i =0  ; i < n ; i  ++){
+        if(stalls[i] - lastPos >= mid){
+            cowCount++;
+            if(cowCount==k){
+                return true;
+            }
+            lastPos = stalls[i];
+        }
+    }
+    return false;
+    
+}
+    int solve(int n, int k, vector<int> &stalls) {
+    
+        // Write your code here
+        
+        sort(stalls.begin() , stalls.end());
+        int s = 0;
+        int maxi = -1;
+        for(int  i =0  ; i < n ;  i ++){
+            maxi = max(maxi , stalls[i]);
+        }
+        
+        int e = maxi;
+        int ans  = -1;
+        int m = s + (e-s)/2;
+        
+        while(s <= e){
+            if(isPossible(stalls , k , m)){
+                ans = m;
+                s =m+1;
+            }else{
+                e = m-1;
+            }
+            
+            m  = s+ (e-s)/2;
+            
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> stalls(n);
+        for (int i = 0; i < n; i++) {
+            cin >> stalls[i];
+        }
+        // char ch;
+        // cin >> ch;
+
+        Solution obj;
+        cout << obj.solve(n, k, stalls) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
