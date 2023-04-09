301e83a5bf3dcadfeb6d5160c8fe0b7771b17450
Kapil <kapilsoni54768161@gmail.com>
2023-04-09 11:19:11 +0530

    .

---
+++ 7. Recursion(Pattern Wise)/7.1/Reverse a Stack.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+
+void helper(stack<int>&St , stack<int>&temp){
+    if(St.empty())return;
+    int top = St.top();
+    St.pop();
+    temp.push(top);
+    helper(St,temp);
+   
+}
+    void Reverse(stack<int> &St){
+        stack<int>temp;
+        helper(St,temp);
+        St = temp;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        stack<int> St;
+        for(int i=0;i<N;i++){
+            int x;
+            cin>>x;
+            St.push(x);
+        }
+        Solution ob;
+        ob.Reverse(St);
+        vector<int>res;
+        while(St.size())
+        {
+            res.push_back(St.top());
+            St.pop();
+        }
+        for(int i = res.size()-1;i>=0;i--)
+        {
+            cout<<res[i]<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
