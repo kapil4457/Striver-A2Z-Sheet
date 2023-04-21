624285781e98bc743df516d892ff0d427bc9249a
Kapil <kapilsoni54768161@gmail.com>
2023-04-21 22:16:49 +0530

    .

---
+++ 8. Bit Manipulation [Concepts & Problems] & Advanced Maths/8.3/Prime Factorization using Sieve.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void sieve() {}
+
+    vector<int> findPrimeFactors(int n) {
+
+        // Write your code here
+        vector<int>ans;
+        
+        for(int i = 2 ; i <= n ; i++){
+            
+            while(n%i==0){
+                ans.push_back(i);
+                n/=i;
+            }
+        }
+        
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        int n;
+        cin >> n;
+
+        Solution obj;
+        obj.sieve();
+        vector<int> vec = obj.findPrimeFactors(n);
+        for (int i = 0; i < vec.size(); i++) {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
