a97be7da88207f73621311d686c05a27f0c154f4
Kapil <kapilsoni54768161@gmail.com>
2023-05-09 16:23:16 +0530

    .

---
+++ 12. Greedy Algorithm/12.1/Assign Cookies.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxChildren(int N, int M, vector<int> &g, vector<int> &s) {
+   sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i = 0, j=0;
+    while(i<g.size() && j<s.size()){
+            if(s[j]>=g[i])
+                i++; // when the child get the cookie, foward child by 1
+            j++;
+        }
+        return i;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+
+        vector<int> greed(N), sz(M);
+        for (int i = 0; i < N; i++) {
+            cin >> greed[i];
+        }
+        for (int i = 0; i < M; i++) {
+            cin >> sz[i];
+        }
+        Solution obj;
+        cout << obj.maxChildren(N, M, greed, sz) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
