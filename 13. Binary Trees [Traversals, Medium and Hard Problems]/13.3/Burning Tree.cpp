dd7bf0b2a1380584811c34a7988c3574fdaef1d6
Kapil <kapilsoni54768161@gmail.com>
2023-06-02 21:07:16 +0530

    .

---
+++ 13. Binary Trees [Traversals, Medium and Hard Problems]/13.3/Burning Tree.cpp
@@ -0,0 +1,216 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+    
+    private : 
+    queue<Node *>q;
+    unordered_map<Node * , Node *>parent_node;
+    unordered_map<Node *  , bool> visited;
+    Node* targetNode = NULL;
+  public:
+  
+  void makeParent(Node * root){
+      q.push(root);
+      while(!q.empty()){
+          root = q.front();
+          q.pop();
+          
+          
+          if(root->left){
+              q.push(root->left);
+              parent_node[root->left]=root;
+          }
+          
+          if(root->right){
+              q.push(root->right);
+              parent_node[root->right]=root;
+          }
+      }
+  }
+  
+  
+  void findTargetNode(Node * root , int target){
+  
+      if(root==NULL)return;
+      
+      if(root->data==target){
+          targetNode = root;
+          return;
+          
+      }
+      
+      findTargetNode(root->left , target);
+      findTargetNode(root->right , target);
+      
+  }
+  
+  
+    int minTime(Node* root, int target) 
+    {
+        // Your code goes here
+        
+        makeParent(root);
+        findTargetNode(root , target);
+        visited[targetNode]=true;
+        int currDistance =0;
+        
+        q.push(targetNode);
+        while(!q.empty()){
+            int size = q.size();
+                        currDistance++;
+
+            for(int i =0 ; i < size ; i ++){
+                
+                Node * current  = q.front();
+                q.pop();
+                
+                
+                 
+                    if(current->left && !visited[current->left]){
+                        q.push(current->left);
+                        visited[current->left] = true;
+                    }
+                    
+                    
+                    if(current->right && !visited[current->right]){
+                        q.push(current->right);
+                        visited[current->right] = true;
+                        
+                    }
+                    
+                    
+                    if(parent_node[current] && !visited[parent_node[current]]){
+                        q.push(parent_node[current]);
+                        visited[parent_node[current]]=true;
+                    }
+            }
+            
+        }
+        
+        
+        return currDistance-1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) 
+    {    
+        string treeString;
+        getline(cin, treeString);
+        // cout<<treeString<<"\n";
+        int target;
+        cin>>target;
+        // cout<<target<<"\n";
+
+        Node *root = buildTree(treeString);
+        Solution obj;
+        cout<<obj.minTime(root, target)<<"\n"; 
+
+        cin.ignore();
+
+    }
+
+
+    return 0;
+}
+
+// } Driver Code Ends
