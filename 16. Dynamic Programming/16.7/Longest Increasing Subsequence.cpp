9c3d00999ccfec05a4e29410f3d24254f7b84149
Kapil <kapilsoni54768161@gmail.com>
2023-07-08 23:17:11 +0530

    .

---
+++ 16. Dynamic Programming/16.7/Longest Increasing Subsequence.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find length of longest increasing subsequence.
+ int f(int ind , int prev_ind , int arr[] , int n , vector<vector<int>>&dp){
+     if(n==ind)return 0;
+     
+     if(dp[ind][prev_ind+1]!=-1)return dp[ind][prev_ind+1];
+     int len = 0 + f(ind+1 , prev_ind , arr , n , dp);
+     if(prev_ind==-1 || arr[prev_ind] < arr[ind]){
+         len = max(len ,  1 + f(ind+1 , ind , arr , n , dp));
+     }
+     return dp[ind][prev_ind+1]=len;
+     
+ }
+    int longestSubsequence(int n, int a[])
+    {
+       // your code here
+       vector<vector<int>>dp(n , vector<int>(n+1 , -1));
+      return f(0,-1,a,n , dp);
+    
+       
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        //taking size of array
+        cin>>n;
+        int a[n];
+        
+        //inserting elements to the array
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        //calling method longestSubsequence()
+        cout << ob.longestSubsequence(n, a) << endl;
+    }
+}
+
+// } Driver Code Ends
