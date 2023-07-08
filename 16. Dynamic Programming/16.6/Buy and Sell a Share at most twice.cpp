53573b33bf234325c72f167a599b8ba7687f639a
Kapil <kapilsoni54768161@gmail.com>
2023-07-08 21:41:56 +0530

    .

---
+++ 16. Dynamic Programming/16.6/Buy and Sell a Share at most twice.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+int getAns(vector<int>& Arr, int n, int ind, int buy, int cap, vector<vector<vector<int>>>& dp ){
+
+    if(ind==n || cap==0) return 0; //base case
+    
+    if(dp[ind][buy][cap]!=-1)
+        return dp[ind][buy][cap];
+        
+    int profit;
+    
+    if(buy==0){// We can buy the stock
+        profit = max(0+getAns(Arr,n,ind+1,0,cap,dp), 
+                    -Arr[ind] + getAns(Arr,n,ind+1,1,cap,dp));
+    }
+    
+    if(buy==1){// We can sell the stock
+        profit = max(0+getAns(Arr,n,ind+1,1,cap,dp),
+                    Arr[ind] + getAns(Arr,n,ind+1,0,cap-1,dp));
+    }
+    
+    return dp[ind][buy][cap] = profit;
+}
+  
+
+
+int maxProfit(vector<int>&prices){
+    //Write your code here..
+   
+    
+      int n = prices.size();
+         vector<vector<vector<int>>> dp(n,
+                                    vector<vector<int>> 
+                                            (2,vector<int>(3,-1)));
+    
+    return getAns(prices,n,0,0,2,dp);
+    
+}
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        vector<int> price(n);
+        for(int i=0;i<n;i++) cin>>price[i];
+        cout<<maxProfit(price)<<endl;
+    }
+
+}
+
+// } Driver Code Ends
